566d329b720db9428e7adf33021c9a8024d51fd4
igorSanthus <rogigor08@gmail.com>
2017-06-21 10:55:17 -0300

    Add files via upload

---
+++ passagem.c
@@ -0,0 +1,18 @@
+#include<stdlib.h>
+#include<stdio.h>
+main(){
+float ID;
+printf("Informe sua Idade:");
+scanf("%f",&ID);
+
+if(ID>=60){
+     printf("Voce Tem Direito A Passe Livre");
+        }else{ if(ID<=7){
+
+    printf("Voce Tem Direito A Passe Livre");
+
+}else{
+ printf("Voce Nao Tem Direito A Passe Livre");}
+
+}
+}
